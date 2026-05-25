6391aeb7eb1aae17e1a14329fd288087873f696f
Mohd Shami <shamisheikh1786@gmail.com>
2026-05-25 11:42:30 +0530

    Add solution for Jump Game VII problem
    Implement the Jump Game VII solution to determine if the last index can be reached.

--- 1871_JumpGameVII.py
+++ 1871_JumpGameVII.py
@@ -1,1 +1,52 @@
+'''
+1871. Jump Game VII
 
+You are given a 0-indexed binary string s and two integers minJump and maxJump. In the beginning, you are standing at index 0, which is equal to '0'. You can move from index i to index j if the following conditions are fulfilled:
+
+i + minJump <= j <= min(i + maxJump, s.length - 1), and
+s[j] == '0'.
+Return true if you can reach index s.length - 1 in s, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "011010", minJump = 2, maxJump = 3
+Output: true
+Explanation:
+In the first step, move from index 0 to index 3. 
+In the second step, move from index 3 to index 5.
+Example 2:
+
+Input: s = "01101110", minJump = 2, maxJump = 3
+Output: false
+ 
+
+Constraints:
+
+2 <= s.length <= 105
+s[i] is either '0' or '1'.
+s[0] == '0'
+1 <= minJump <= maxJump < s.length
+'''
+
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        
+        queue = [0]
+        farthest = 0
+        
+        for i in queue:
+            start = max(i + minJump, farthest + 1)
+            end = min(i + maxJump, n - 1)
+            
+            for j in range(start, end + 1):
+                if s[j] == '0':
+                    if j == n - 1:
+                        return True
+                    queue.append(j)
+            
+            farthest = end
+        
+        return n == 1
