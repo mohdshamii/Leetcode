99e267276080f6806cb667e8732e2edb7264df92
Mohd Shami <shamisheikh1786@gmail.com>
2026-06-01 12:47:42 +0530

    Add rotateRight method for linked list rotation
    Implement rotation of a linked list to the right by k places.

--- 61_RotateList.py
+++ 61_RotateList.py
@@ -1,1 +1,59 @@
+'''
+61. Rotate List
 
+premium lock icon
+Companies
+Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+'''
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+
+        # Find length and last node
+        n = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            n += 1
+
+        k %= n
+        if k == 0:
+            return head
+
+        # Make circular list
+        tail.next = head
+
+        # Find new tail
+        steps = n - k - 1
+        new_tail = head
+        for _ in range(steps):
+            new_tail = new_tail.next
+
+        # New head
+        new_head = new_tail.next
+
+        # Break circle
+        new_tail.next = None
+
+        return new_head
