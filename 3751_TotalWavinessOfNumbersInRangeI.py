817ebbff9194201ab982f2bc250a73c4d6988ff7
Mohd Shami <shamisheikh1786@gmail.com>
2026-06-04 20:08:47 +0530

    Implement totalWaviness method in Solution class

--- 3751_TotalWavinessOfNumbersInRangeI.py
+++ 3751_TotalWavinessOfNumbersInRangeI.py
@@ -58,3 +58,21 @@ Constraints:
 
 1 <= num1 <= num2 <= 105
 '''
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        def waviness(x):
+            s = str(x)
+            cnt = 0
+
+            for i in range(1, len(s) - 1):
+                if (s[i] > s[i - 1] and s[i] > s[i + 1]) or \
+                   (s[i] < s[i - 1] and s[i] < s[i + 1]):
+                    cnt += 1
+
+            return cnt
+
+        ans = 0
+        for x in range(num1, num2 + 1):
+            ans += waviness(x)
+
+        return ans
