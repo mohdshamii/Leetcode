b3c286273fabb28892f825f1c25a844e02586f15
Mohd Shami <shamisheikh1786@gmail.com>
2026-06-12 22:22:45 +0530

    Implement edge weight assignment for tree queries

--- 3559_NumberOfWaysToAssignEdgeWeightsII.py
+++ 3559_NumberOfWaysToAssignEdgeWeightsII.py
@@ -58,3 +58,74 @@ queries[i] == [ui, vi]
 1 <= ui, vi <= n
 edges represents a valid tree.
 '''
+from typing import List
+import math
+
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        MOD = 10**9 + 7
+
+        n = len(edges) + 1
+
+        g = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+
+        LOG = math.ceil(math.log2(n)) + 1
+
+        up = [[0] * LOG for _ in range(n + 1)]
+        depth = [0] * (n + 1)
+
+        stack = [(1, 0)]
+        while stack:
+            u, p = stack.pop()
+            up[u][0] = p
+
+            for j in range(1, LOG):
+                up[u][j] = up[up[u][j - 1]][j - 1]
+
+            for v in g[u]:
+                if v == p:
+                    continue
+                depth[v] = depth[u] + 1
+                stack.append((v, u))
+
+        def lca(a, b):
+            if depth[a] < depth[b]:
+                a, b = b, a
+
+            diff = depth[a] - depth[b]
+            bit = 0
+            while diff:
+                if diff & 1:
+                    a = up[a][bit]
+                diff >>= 1
+                bit += 1
+
+            if a == b:
+                return a
+
+            for j in range(LOG - 1, -1, -1):
+                if up[a][j] != up[b][j]:
+                    a = up[a][j]
+                    b = up[b][j]
+
+            return up[a][0]
+
+        pow2 = [1] * n
+        for i in range(1, n):
+            pow2[i] = (pow2[i - 1] * 2) % MOD
+
+        ans = []
+
+        for u, v in queries:
+            w = lca(u, v)
+            k = depth[u] + depth[v] - 2 * depth[w]
+
+            if k == 0:
+                ans.append(0)
+            else:
+                ans.append(pow2[k - 1])
+
+        return ans
