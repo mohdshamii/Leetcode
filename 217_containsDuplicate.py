b5ddcd5ebea070a8aadc1d0c465414cf7b3d2436
Mohd Shami <shamisheikh1786@gmail.com>
2026-07-02 18:48:09 +0530

    Add containsDuplicate methods for duplicate checking
    Implemented two methods to check for duplicates in a list: one using a set for efficiency and another brute force method.

---
+++ 217_containsDuplicate.py
@@ -0,0 +1,22 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+'’’
+#brute force
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j]:
+                    return True
+
+        return False
