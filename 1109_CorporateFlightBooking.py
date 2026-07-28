0758713fdc47fa4a24c8540ccf08a6b8c7f095b6
Mohd Shami <shamisheikh1786@gmail.com>
2026-07-28 15:36:27 +0530

    Create 1109_CorporateFlightBooking.py

---
+++ 1109_CorporateFlightBooking.py
@@ -0,0 +1,16 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        diff = [0] * (n + 1)
+
+        for first, last, seats in bookings:
+            diff[first - 1] += seats
+            if last < n:
+                diff[last] -= seats
+
+        ans = [0] * n
+        ans[0] = diff[0]
+
+        for i in range(1, n):
+            ans[i] = ans[i - 1] + diff[i]
+
+        return ans
