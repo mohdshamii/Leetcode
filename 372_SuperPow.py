22313e9aa738a4f4e765043df779e4abfc44d927
Mohd Shami <shamisheikh1786@gmail.com>
2026-08-08 22:19:09 +0530

    Implement superPow function in 372_SuperPow.py

---
+++ 372_SuperPow.py
@@ -0,0 +1,8 @@
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        result = 1
+
+        for digit in b:
+            result = (pow(result, 10, 1337) * pow(a, digit, 1337)) % 1337
+
+        return result
