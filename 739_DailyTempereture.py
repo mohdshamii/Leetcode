cda75940c0831bb6ca4640a241f3e4575b04ec23
Mohd Shami <shamisheikh1786@gmail.com>
2026-08-23 11:55:50 +0530

    Implement dailyTemperatures function in Solution class

---
+++ 739_DailyTempereture.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        stack = []
+        ans = [0] * n
+
+        for i in range(n):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                j = stack.pop()
+                ans[j] = i - j
+
+            stack.append(i)
+
+        return ans
