f651e716ced7214508d37ed853088a0273bc1627
Mohd Shami <shamisheikh1786@gmail.com>
2026-08-23 12:00:13 +0530

    Implement nextGreaterElement function in Solution class

---
+++ 496_NextGreaterElementI.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        nge = {}
+
+        for num in nums2:
+            while stack and stack[-1] < num:
+                nge[stack.pop()] = num
+
+            stack.append(num)
+
+        return [nge.get(num, -1) for num in nums1]
