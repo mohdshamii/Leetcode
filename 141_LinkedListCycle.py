530ab8d2e1a2144c4b1ce69fdc1e19b9ecb61752
Mohd Shami <shamisheikh1786@gmail.com>
2026-08-26 16:40:45 +0530

    Implement cycle detection in linked list

---
+++ 141_LinkedListCycle.py
@@ -0,0 +1,13 @@
+hereclass Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
