ab35e84d03c081048012d3b909225552a6805000
Mohd Shami <shamisheikh1786@gmail.com>
2026-09-04 09:31:03 +0530

    Implement matrix transposition function

---
+++ 867_TransposeOfMatrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m = len(matrix)
+        n = len(matrix[0])
+        ans = [[0] * m for _ in range(n)]
+        for r in range(m):
+            for c in range(n):
+                ans[c][r] = matrix[r][c]
+        return ans
