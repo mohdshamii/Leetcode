cabb571878ff1e452c6b4fef89f1647909785763
Mohd Shami <shamisheikh1786@gmail.com>
2026-09-08 09:43:13 +0530

    Implement intersection method for two arrays

---
+++ 350_IntersectionOfTwoArrayll.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intersect(self, nums1, nums2):
+        ans = []
+
+        for x in nums1:
+            if x in nums2:
+                ans.append(x)
+                nums2.remove(x)
+
+        return ans
